78aa8534b1f1418f7577db08260bc903eb783d9e
Steven Duzevich <steven@duz.ie>
2023-12-12 15:37:34 +0100

    DEPLOY better db

--- plotmyvert_backend/plotmyvert_backend/settings.py
+++ plotmyvert_backend/plotmyvert_backend/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = 'plotmyvert_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db/db.sqlite3',
     }
 }
 
